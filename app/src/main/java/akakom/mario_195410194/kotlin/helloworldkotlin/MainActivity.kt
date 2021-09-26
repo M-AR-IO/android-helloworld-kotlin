6b5ec6b60ca9731d7f6d45750cdf1521e82fff3d
M-AR-IO <ardamomo3@gmail.com>
2021-09-26 17:45:29 +0700

    Awal project android studio - empty activity

---
+++ app/src/main/java/akakom/mario_195410194/kotlin/helloworldkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package akakom.mario_195410194.kotlin.helloworldkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
